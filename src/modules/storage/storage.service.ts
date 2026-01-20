74a4c8485796ebce38dc80860009fe0f613ec63a
DOC <nushajon@gmail.com>
2026-01-20 16:40:57 +0500

    trim

--- src/modules/storage/storage.service.ts
+++ src/modules/storage/storage.service.ts
@@ -6,9 +6,9 @@ const streamifier = require('streamifier');
 export class StorageService {
   constructor() {
     cloudinary.config({
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME?.trim(),
+      api_key: process.env.CLOUDINARY_API_KEY?.trim(),
+      api_secret: process.env.CLOUDINARY_API_SECRET?.trim(),
     });
   }
 
